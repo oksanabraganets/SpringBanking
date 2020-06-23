a7ac0bc38a1c4d1dab8872651642f9c774b50f18
oksana <oksanaov@gmail.com>
2020-06-23 22:12:05 +0300

    code to generate bills

--- src/test/java/com/example/SpringBanking/SpringBankingApplicationTests.java
+++ src/test/java/com/example/SpringBanking/SpringBankingApplicationTests.java
@@ -1,13 +1,42 @@
 package com.example.SpringBanking;
 
+import com.example.SpringBanking.entity.BillDAO;
+import com.example.SpringBanking.entity.Role;
+import com.example.SpringBanking.repository.BillRepository;
+import com.example.SpringBanking.repository.UserRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @SpringBootTest
 class SpringBankingApplicationTests {
 
+	private final BillRepository billRepository;
+	private final UserRepository userRepository;
+
+	@Autowired
+	public SpringBankingApplicationTests(BillRepository billRepository, UserRepository userRepository) {
+		this.billRepository = billRepository;
+		this.userRepository = userRepository;
+	}
+
 	@Test
-	void contextLoads() {
+	void createBills() {
+		List<Long> users = userRepository.findAll().stream()
+				.filter(x -> x.getRole() == Role.ROLE_USER)
+				.map(x -> x.getId())
+				.collect(Collectors.toList());
+		for (Long userId : users){
+			BillDAO bill = BillDAO.builder()
+					.user(userId)
+					.amount(120L)
+					.recipient("VOLIA")
+					.build();
+			billRepository.save(bill);
+		}
 	}
 
 }
