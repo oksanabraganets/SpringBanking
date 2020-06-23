c06e3ca0fb7c5ae457e036a5350535e3ea7b39b4
oksana <oksanaov@gmail.com>
2020-06-23 22:02:34 +0300

    request to approve DTO created

---
+++ src/main/java/com/example/SpringBanking/dto/ApproveCreditDTO.java
@@ -0,0 +1,13 @@
+package com.example.SpringBanking.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@ToString
+public class ApproveCreditDTO {
+    Long requestId;
+}
